a2e36f8582443a06958e567aafb723e4315aafc3
Lauren Stein <lmstein22@gmail.com>
2020-09-17 10:46:19 -0400

    Added index.html doc, imported time to smooth out delay in browser

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -7,11 +7,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017"
 mongo = PyMongo(app)
 
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+db = client.mars_db
 
+mars_db.insert_one(mars_dict)
 
 @app.route("/")
+def index():
+    info = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
+
+
+@app.route("/scrape")
 
 if __name__ == "__main__":
     app.run(debug=True)

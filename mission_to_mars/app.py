e6d9f575117234c37317bff1d3673a7db7ac7406
Lauren Stein <lmstein22@gmail.com>
2020-09-18 11:00:52 -0400

    completed app.py added scrape route, cleaning up html

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -1,25 +1,36 @@
 from flask import Flask, render_template, redirect
-from flask import jsonify
-from .scrape_mars import mars_info
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 
+#Import app.py scrape_mars
+import scrape_mars
+
+#Create an instance of Flask
 app = Flask(__name__)
 
-# Set up mongo connection
-MONGO_URI = "mongodb://localhost:27017/"
-
-client = MongoClient(MONGO_URI)
-
-db = client.mars_db
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def echo():
-    return render_template("index.html", text="Mission to Mars")
+def index():
+    #Store the mars_dictionary in a list
+    mars_info = mongo.db.mars_db.find_one()
+    
+    #Render the index.html template 
+    return render_template("index.html", mars=mars_info)
 
 @app.route("/scrape")
 def scrape():
-    return jsonify(mars_info())
+    #Create a Link to mongodb dict
+    mars_dict = mongo.db.mars_dict
+
+    #Run the scrape_mars.py and store as variable
+    mars_data = scrape_mars.mars_info()
+    
+    # Update the Mongo database using update and upsert=True
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
